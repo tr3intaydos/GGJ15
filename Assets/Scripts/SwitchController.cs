043d202413c54bfd3cd50c29ddbcec56dcf8847b
Guillermo Lopez <tr3intaydos@gmail.com>
2015-01-24 12:44:30 -0600

    Deteniendo Plataformas
    Ahora las plataformas ya se pueden detener, no olvidar ponerle a las
    plataformas el tag de Platforms, se pueden poner mas cosas que tengan un
    animaciones y con ese tag se detendran durante el tiempo que se le
    configuro.

---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchController : MonoBehaviour {
+
+	public float timeForRestart = 5f;
+	
+	private GameObject[] gos;
+
+	void Start(){
+		gos = GameObject.FindGameObjectsWithTag ("Plataforms");
+	}
+
+	void ExecAction(){
+		StopPlatforms();
+	}
+
+	void StartPlatforms(){
+		Debug.Log ("stoping platforms");
+		foreach (GameObject go in gos) {
+			Animator a = go.GetComponent<Animator>();
+			if(a!=null){
+				a.StopPlayback();
+			}
+		}
+	}
+
+	void StopPlatforms(){
+		foreach (GameObject go in gos) {
+			Animator a = go.GetComponent<Animator>();
+			if(a!=null){
+				a.StartPlayback();
+			}
+		}
+		Invoke ("StartPlatforms", timeForRestart);
+	}
+}
---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerController : MonoBehaviour {
+
+	private GameObject go;
+
+	void OnTriggerEnter(Collider col){
+		go = col.gameObject;
+		Debug.Log ("enter");
+	}
+
+	void OnTriggerExit(Collider col){
+		go = null;
+		Debug.Log ("exit");
+	}
+
+	void Update(){
+		if (go == null)
+			return;
+		if (Input.GetKeyUp (KeyCode.F)) {
+			go.SendMessage ("ExecAction", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
